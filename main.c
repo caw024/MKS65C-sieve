19df6f07d74ba75485e464f88867fe876c3d6ee9
Calvin Aw <caw@stuy.edu>
2018-10-21 15:29:05 -0400

    works for n up to 100000 ish

--- main.c
+++ main.c
@@ -2,10 +2,15 @@
 #include <stdlib.h>
 #include <math.h>
 #include "sieve.h"
+#include <time.h>
+//CITATIONS: I found how time.h works from geeksforgeeks.org
 
 int main(int argc, char * argv[]){
+  clock_t t;
+  double cpu_time;
   int iterations = 1;
-  int target = 1000000;
+  int target = 100000;
+  
   if(argc > 1){
     target = atoi(argv[1]);
   }
@@ -16,13 +21,18 @@ int main(int argc, char * argv[]){
 
   int ans = 0;
   while(iterations>0){
-      ans=sieve(target);
-      iterations--;
-      printf("The n=%d prime is %d\n", target, ans );
+    t = clock();
+    ans=sieve(target);
+    t = clock() - t;
+    cpu_time = ((double) t)/CLOCKS_PER_SEC;
+    printf("iteration %d: took %f seconds\n", iterations, cpu_time);
+      
+    iterations--;
+    printf("The n=%d prime is %d\n", target, ans );
 
-      //this is to modify which prime to
-      //potentially avoid CPU caching
-      target++;
+    //this is to modify which prime to
+    //potentially avoid CPU caching
+    target++;
   }
   return 0;
 }
--- sieve.c
+++ sieve.c
@@ -4,24 +4,40 @@
 #include "sieve.h"
 
 int sieve(int targetPrime){
-  int k = (int) (targetPrime * 1.5) + 20;
+  int k =  targetPrime * (int) (1.25 * log(targetPrime)) + 20;
   int arr[k];
-  int i = 2;
+  
   arr[0] = 0;
   arr[1] = 0;
+
+  int i = 2;
+  //setup array
   while (i < k){
     arr[i] = i;
     i++;
   }
+
+  
   int j = 2;
   int m;
-  for (j; j < k; j++){
-    m = j;
-    if (a[j] != 0){
-      for (m; m < sqrt(k); m++){
-	if (a[m] % j == 0){
-	  a[m] = 0;
-	}
+  //sieve of eratosthenes
+  for (j; j <= sqrt(k); j++){
+    if (arr[j] != 0){
+      m = 2*j; //next multiple of j;
+      for (m; m < k; m+=j){
+	 arr[m] = 0;	
+      }
+    }
+  }
+
+  int ctr = 0;
+  int n = 0;
+  //find nonzero values
+  for (n; n < k; n++){
+    if (arr[n] != 0){
+      ctr += 1;
+      if (ctr == targetPrime){
+	return arr[n];
       }
     }
   }
